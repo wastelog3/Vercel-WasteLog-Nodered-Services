e5d7753fca81412c72648cc8023de1eda1b0611e
zhar <azharfauzansip@gmail.com>
2025-07-10 22:36:45 +0700

    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+    const express = require('express');
+    const RED = require("node-red");
+    const http = require('http');
+    
+    const app = express();
+    const server = http.createServer(app);
+    
+    const settings = {
+        httpAdminRoot:"/red",
+        httpNodeRoot: "/api",
+        userDir:"./.node-red",
+        flowFile: 'flows.json',
+        functionGlobalContext: {
+        },
+
+        adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "WasteLog",
+            password: "$2a$12$xTEa1UAwLt3sCL5KjpW2BejnFAJNNt/doi4/gdzw33ZKmR6owXfte",
+            permissions: "*"
+        }]
+    }
+    };
+    
+    RED.init(server,settings);
+    
+    app.use(settings.httpAdminRoot, RED.httpAdmin);
+    app.use(settings.httpNodeRoot, RED.httpNode);
+    
+    server.listen(3000, () => {
+        RED.start();
+        console.log('Node-RED listening on port 3000');
+    });
+    
+    module.exports = app;
