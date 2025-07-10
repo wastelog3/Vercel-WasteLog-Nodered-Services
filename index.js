979d593cd6d2b4676fdc042d8ff0a43b78e6bdfa
wastelog3 <waste.log3@gmail.com>
2025-07-10 22:57:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-    const express = require('express');
-    const RED = require("node-red");
-    const http = require('http');
+const express = require('express');
+const RED = require("node-red");
+const http = require('http');
     
-    const app = express();
-    const server = http.createServer(app);
+const app = express();
+const server = http.createServer(app);
     
-    const settings = {
-        httpAdminRoot:"/red",
-        httpNodeRoot: "/api",
-        userDir:"./.node-red",
-        flowFile: 'flows.json',
-        functionGlobalContext: {
-        },
+const settings = {
+httpAdminRoot:"/red",
+httpNodeRoot: "/api",
+    userDir:"./.node-red",
+    flowFile: 'flows.json',
+    functionGlobalContext: {
+    },
 
-        adminAuth: {
-        type: "credentials",
-        users: [{
-            username: "WasteLog",
-            password: "$2a$12$xTEa1UAwLt3sCL5KjpW2BejnFAJNNt/doi4/gdzw33ZKmR6owXfte",
-            permissions: "*"
-        }]
-    }
-    };
-    
-    RED.init(server,settings);
-    
-    app.use(settings.httpAdminRoot, RED.httpAdmin);
-    app.use(settings.httpNodeRoot, RED.httpNode);
-    
-    RED.start();
-    
-    module.exports = app;
+    adminAuth: {
+    type: "credentials",
+    users: [{
+        username: "WasteLog",
+        password: "$2a$12$xTEa1UAwLt3sCL5KjpW2BejnFAJNNt/doi4/gdzw33ZKmR6owXfte",
+        permissions: "*"
+    }]
+}
+};
+
+RED.init(server,settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+RED.start();
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Hello from my custom API!' });
+});
+
+module.exports = app;
